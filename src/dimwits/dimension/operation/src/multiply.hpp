2fd3762a721075252259bbe7c02516c8ec51d193
Austin McCartney <amccartney@lanl.gov>
2016-10-23 16:35:08 -0600

    compilation time test

--- src/dimwits/dimension/operation/src/multiply.hpp
+++ src/dimwits/dimension/operation/src/multiply.hpp
@@ -4,60 +4,19 @@ auto multiply( Type< Left >, Type< Right > ){
   auto left = Left{};
   auto right = Right{};
 
-  auto leftDimensions = hana::to_set( hana::keys( left ) );
-  
-  auto rightDimensions = hana::to_set( hana::keys( right ) );
-
-  auto leftExclusiveDimensions =
-    hana::difference( leftDimensions, rightDimensions );
-
-  auto rightExclusiveDimensions =
-    hana::difference( rightDimensions, leftDimensions );
-
-  auto makeCondition = []( auto t ){
-    return [t]( auto element ){
-      return t ^hana::contains^ hana::first(element);
-    };
-  };
-
-  auto conditionalInsert = []( auto condition ){
-    return [ condition ]( auto map, auto element ){
-      return hana::if_
-      ( condition( element ),
-        hana::insert( map, element ), map );
-    };
-  };
-  
-  auto merge = [](auto m1, auto m2) {
-    return hana::fold_left( m2, m1, hana::insert );
-  };
-  
-  auto leftExclusive = 
-    hana::fold_left
-    ( left, hana::make_map(), conditionalInsert
-      ( makeCondition( leftExclusiveDimensions ) ) );
-  
-  auto rightExclusive =
-    hana::fold_left
-    ( right, hana::make_map(), conditionalInsert
-      ( makeCondition( rightExclusiveDimensions ) ) );
-  
-  auto mergedExclusives = merge( leftExclusive, rightExclusive );
-
-  auto sharedDimensions =
-    hana::intersection( leftDimensions, rightDimensions );
+  auto dimensions =
+    hana::union_( hana::to_set( hana::keys( left ) ),
+		  hana::to_set( hana::keys( right ) ) );
    
   auto sum = [=]( auto map, auto dimension ){
-    auto sum = left[dimension] + right[dimension];
-    return hana::insert( map, hana::make_pair( dimension, sum ) );
+    auto sum =
+    hana::find( left, dimension ).value_or( Ratio<0> )
+    + hana::find( right, dimension ).value_or( Ratio<0> );
+
+    return hana::if_( hana::bool_c< sum == Ratio<0> >, map,
+		      hana::insert( map, hana::make_pair( dimension, sum ) ) );
   };
   
-  auto full = hana::fold_left( sharedDimensions, hana::make_map(), sum );
-  auto reduce = [=]( auto map, auto dimension ){
-    return hana::if_( hana::bool_c< hana::second( dimension ) == Ratio<0> >,
-                      hana::erase_key( map, hana::first( dimension ) ), map );
-  };
-  auto clean = hana::fold_left( full, full, reduce );
-  auto result = merge( clean, mergedExclusives );
+  auto result = hana::fold_left( dimensions, hana::make_map(), sum );
   return Type< decltype(result) >{};
 }
--- src/dimwits/unit/operation/src/multiply.hpp
+++ src/dimwits/unit/operation/src/multiply.hpp
@@ -1,51 +1,23 @@
 template< typename Left, typename Right >
 auto multiply( Type<Left>, Type<Right> ){
+  
   auto left = Left{};
   auto right = Right{};
 
-  auto leftUnits = 
-    hana::to<hana::set_tag>( hana::keys( left ) );
-  auto rightUnits =
-    hana::to<hana::set_tag>( hana::keys( right ) );
+  auto units =
+    hana::union_( hana::to_set( hana::keys( left ) ),
+		  hana::to_set( hana::keys( right ) ) );
 
-  auto leftExclusiveUnits =
-    hana::difference( leftUnits, rightUnits );
-  auto rightExclusiveUnits =
-     hana::difference( rightUnits, leftUnits );
-
-  auto makeCondition = []( auto t ){
-    return [t]( auto element ){
-      return t ^hana::contains^ hana::first(element);
-    };
-  };
-
-  auto merge = [](auto m1, auto m2) {
-    return hana::fold_left(m1, m2, hana::insert);
-  };
-   
-  auto mergedExclusives = 
-    merge( hana::to<hana::map_tag>
-           ( hana::filter
-             ( hana::to<hana::tuple_tag>(left), 
-               makeCondition( leftExclusiveUnits ) ) ),
-           hana::to<hana::map_tag>
-           ( hana::filter
-             ( hana::to<hana::tuple_tag>(right), 
-               makeCondition( rightExclusiveUnits ) ) ) );
-
-  auto sharedUnits =
-    hana::intersection( leftUnits, rightUnits );
-   
   auto sum = [=]( auto map, auto unit ){
-    auto sum = left[unit] + right[unit];
-    return hana::insert( map, hana::make_pair( unit, sum ) );
+    auto sum =
+    hana::find( left, unit ).value_or( Ratio<0> )
+    + hana::find( right, unit ).value_or( Ratio<0> );
+
+    return hana::if_( hana::bool_c< sum == Ratio<0> >, map,
+		      hana::insert( map, hana::make_pair( unit, sum ) ) );
   };
-  auto full = hana::fold_left( sharedUnits, hana::make_map(), sum );
-  auto reduce = [=]( auto map, auto unit ){
-    return hana::if_( hana::bool_< std::decay_t< decltype( hana::second( unit ) ) >{} == Ratio<0> >{},
-                      hana::erase_key( map, hana::first( unit ) ), map );
-  };
-  auto clean = hana::fold_left( full, full, reduce );
-  auto result = merge( clean, mergedExclusives );
+
+  auto result = hana::fold_left( units, hana::make_map(), sum );
+  
   return Type< decltype(result) >{};
 }
