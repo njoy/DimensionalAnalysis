aa720e0c1c822526c447257c4860dbdcc40fc35e
Austin McCartney <amccartney@lanl.gov>
2017-04-10 16:15:23 -0600

    updated

--- .travis/build.sh
+++ .travis/build.sh
@@ -6,28 +6,38 @@ if [ "$TRAVIS_OS_NAME" = "linux" ]; then
   sudo update-alternatives --config gcc
   sudo update-alternatives --config clang
   if [ "$CXX" = "clang++" ]; then
-    export appended_flags=$appended_flags"-stdlib=libstdc++"
     export PATH=/usr/bin:$PATH
-    export NOPE='-D no_link_time_optimization=TRUE'
+    export NOPE='-D link_time_optimization=OFF'
+  else
+    export NOPE='-D link_time_optimization=ON'
   fi;
+  if [ "$build_type" = "coverage" ]; then
+    export build_type=DEBUG
+    export coverage=true
+    export NOPE="$NOPE -D coverage=ON"
+  else
+    export coverage=false
+  fi; 
 fi
 
+./fetch_subprojects.py
 mkdir build
 cd build
-cmake -D build_type=$build_type \
+cmake -D CMAKE_BUILD_TYPE=$build_type \
       -D static_libraries=$static_libraries \
-      -D appended_flags="$appended_flags" \
+      -D dimwits_appended_flags="$appended_flags" \
       $NOPE ..
-make -j2
+make VERBOSE=1 -j2
 export COMPILATION_FAILURE=$?
 ctest --output-on-failure -j2
 export TEST_FAILURE=$?
-if [ "$build_type" = "coverage" ]
-then
+
+if $coverage; then
     pip install --user cpp-coveralls
     echo "loading coverage information"
     coveralls  --exclude-pattern "/usr/include/.*|.*/CMakeFiles/.*|.*subprojects.*|.*dependencies.*|.*test\.cpp" --root ".." --build-root "." --gcov-options '\-lp'
 fi
+
 if [ $COMPILATION_FAILURE -ne 0 ] || [ $TEST_FAILURE -ne 0 ];
 then
     exit 1
