44f7e5a07d7f25bbbfc26989cdc18566adca8bf3
Austin McCartney <amccartney@lanl.gov>
2017-07-28 07:39:05 -0600

    no need to repeat myself

--- src/dimwits/ratio/src/greatestCommonFactor.hpp
+++ src/dimwits/ratio/src/greatestCommonFactor.hpp
@@ -2,7 +2,6 @@
 constexpr int64_t greatestCommonFactor( int64_t t1, int64_t t2 ){
   return
     ( t1 == 0 ) ? 1 :
-    ( t1 < 0 )  ? greatestCommonFactor( -t1, t2 ) :
     ( t2 < 0 )  ? greatestCommonFactor( t1, -t2 ) :
     ( t1 < t2 ) ? greatestCommonFactor( t2, t1 ) :
     ( t2 < 2 )  ? 1:
