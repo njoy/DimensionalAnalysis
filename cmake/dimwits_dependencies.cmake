8f0aa049f6924619d2ba75f4e16d5f03ab9a5767
Nathan Gibson <ngibson@lanl.gov>
2020-05-21 14:59:49 -0600

    Added GIT_SHALLOW

--- cmake/dimwits_dependencies.cmake
+++ cmake/dimwits_dependencies.cmake
@@ -13,11 +13,13 @@ include( FetchContent )
 FetchContent_Declare( catch-adapter
     GIT_REPOSITORY  http://github.com/njoy/catch-adapter
     GIT_TAG         origin/build/fetchcontent
+    GIT_SHALLOW     TRUE
     )
 
 FetchContent_Declare( hana-adapter
     GIT_REPOSITORY  http://github.com/njoy/hana-adapter
     GIT_TAG         origin/build/fetchcontent
+    GIT_SHALLOW     TRUE
     )
 
 
